c11d1ad3e55af61d6233170137a6d9adacf6dad9
Mason <ab300819@163.com>
2021-02-02 23:26:12 +0800

    feat: sqlite crud

--- src/core.cpp
+++ src/core.cpp
@@ -8,8 +8,10 @@
 #include <QSqlQuery>
 #include <string>
 
-bool createTable(QSqlQuery database, std::string tableName);
+bool createTable(QSqlQuery query, std::string tableName);
 bool insertData(QSqlQuery query, std::string data);
+int selectAllDate(QSqlQuery query);
+void deleteFromData(QSqlQuery query);
 
 int main(int argc, char* argv[]) {
   QString connectName = "hosts_sqlite";
@@ -47,14 +49,18 @@ int main(int argc, char* argv[]) {
   bool insertResult = insertData(query, "1");
   if (!insertResult) {
     qDebug() << "insert occur error";
+    return EXIT_FAILURE;
   }
+
+  selectAllDate(query);
+  deleteFromData(query);
 
   database.close();
   return EXIT_SUCCESS;
 }
 
 bool createTable(QSqlQuery query, std::string tableName) {
-  //  ½¨±íÓï¾ä
+  //  å»ºè¡¨è¯­å¥
   auto createTableSql = R"~(create table hosts(
     id       INTEGER not null primary key autoincrement,
     group_id INTEGER not null default 0,
@@ -73,25 +79,56 @@ bool createTable(QSqlQuery query, std::string tableName) {
     return true;
   }
 }
+
 bool insertData(QSqlQuery query, std::string data) {
   QString insertData =
       "INSERT INTO hosts(group_id, name, domain, ip, flag) VALUES (?,?,?,?,?)";
-  query.prepare(insertData);
   for (int i = 0; i < 10; ++i) {
+    query.prepare(insertData);
     query.addBindValue("qwertyui");
-    query.addBindValue("²âÊÔ");
+    query.addBindValue("æµ‹è¯•");
     query.addBindValue("localhost");
     query.addBindValue(QString::fromStdString("10.0.0." + std::to_string(i)));
     query.addBindValue(false);
 
     bool insertResult = query.exec();
-    if (insertResult) {
-      return true;
-    } else {
+    if (!insertResult) {
       qDebug() << query.lastError();
-      return false;
+      continue;
     }
   }
 
-  return false;
+  return true;
+}
+
+int selectAllDate(QSqlQuery query) {
+  QString selectSql = "select * from hosts";
+  if (!query.exec(selectSql)) {
+    qDebug() << query.lastError();
+  } else {
+    while (query.next()) {
+      int id = query.value(0).toInt();
+      QString name = query.value(1).toString();
+      QString domain = query.value(3).toString();
+      QString ip = query.value(4).toString();
+      qDebug() << QString("id:%1, name:%2, domain:%3, ip:%4")
+                      .arg(id)
+                      .arg(name)
+                      .arg(domain)
+                      .arg(ip);
+    }
+  }
+
+  return 0;
+}
+
+void deleteFromData(QSqlQuery query) {
+  QString deleteSql = "delete from hosts where id=?";
+  query.prepare(deleteSql);
+  query.addBindValue(22);
+  if (!query.exec()) {
+    qDebug() << query.lastError();
+  } else {
+    qDebug() << "deleted!";
+  }
 }
